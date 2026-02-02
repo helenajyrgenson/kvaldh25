3032b688837e25d3a83a90542fc752d215958c87
helenajyrgenson <helenajyrgenson44@gmail.com>
2026-02-02 14:05:44 +0000

    save2

--- test1.py
+++ test1.py
@@ -1,6 +1,6 @@
 from scipy.stats import ttest_ind
 print(ttest_ind((3,5,4), (12,16,14)))
-print(ttest_ind(3,5,4,6,5,4), (12,16,14,16,15,14))
-print(ttest_ind(13,15,14), 12,16,14)
+print(ttest_ind((3,5,4,6,5,4), (12,16,14,16,15,14)))
+print(ttest_ind((13,15,14), (12,16,14))) 
 
 # pip install scipy
--- test2.py
+++ test2.py
@@ -1,14 +1,23 @@
-lause1 "Kui arno isaga koolimajja jõudis olid tunnid juba alanud"
-lause2 "Mu isamaa mu õnn ja rõõm, kui kaunis oled sa "
+from scipy.stats import ttest_ind
+import string
 
-#Leia t testi abil, kas lausete sõnade keskmine pikkus erineb üldistavalt
-sonad1=lause1.split()
-print(sonad1)
-print (sonad1 [3])
-print(len("tere"))
-#Kuva sõna 3 tähtede arv
-print(len(sonad1[3]))
-print(len(sonad1)) #sõnu lausetes kokku
-sonapikkused1=[len(sona) for sona in sonad1]
-print(sonapikkused1)
-sonapikkused2=[len(sona) for ]
+lause1 = "Kui arno isaga koolimajja jõudis olid tunnid juba alanud"
+lause2 = "Mu isamaa mu õnn ja rõõm, kui kaunis oled sa"
+
+def sonapikkused(lause: str):
+    sonad = lause.split()
+    # eemaldame kirjavahemärgid sõnade lõpust/algusest (nt "rõõm," -> "rõõm")
+    sonad = [s.strip(string.punctuation) for s in sonad]
+    return [len(s) for s in sonad]
+
+pikkused1 = sonapikkused(lause1)
+pikkused2 = sonapikkused(lause2)
+
+print("Lause1 sõnapikkused:", pikkused1)
+print("Lause2 sõnapikkused:", pikkused2)
+
+tulemus = ttest_ind(pikkused1, pikkused2, equal_var=False)  # Welch t-test
+print("T-test tulemus:", tulemus)
+
+print("Keskmine lause1:", sum(pikkused1)/len(pikkused1))
+print("Keskmine lause2:", sum(pikkused2)/len(pikkused2))
