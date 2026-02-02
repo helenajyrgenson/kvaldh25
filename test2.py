7a8b8fd97a96f86cfef90847df19cb29bad313b9
helenajyrgenson <helenajyrgenson44@gmail.com>
2026-02-02 13:40:41 +0000

    save1

---
+++ test2.py
@@ -0,0 +1,14 @@
+lause1 "Kui arno isaga koolimajja jõudis olid tunnid juba alanud"
+lause2 "Mu isamaa mu õnn ja rõõm, kui kaunis oled sa "
+
+#Leia t testi abil, kas lausete sõnade keskmine pikkus erineb üldistavalt
+sonad1=lause1.split()
+print(sonad1)
+print (sonad1 [3])
+print(len("tere"))
+#Kuva sõna 3 tähtede arv
+print(len(sonad1[3]))
+print(len(sonad1)) #sõnu lausetes kokku
+sonapikkused1=[len(sona) for sona in sonad1]
+print(sonapikkused1)
+sonapikkused2=[len(sona) for ]
