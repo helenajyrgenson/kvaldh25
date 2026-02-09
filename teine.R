4fcb3f115b9c0e754f7b81f0c0ac35629c2945a3
helenajyrgenson <helenajyrgenson44@gmail.com>
2026-02-09 13:44:18 +0000

    save 8, r-test

---
+++ teine.R
@@ -0,0 +1,4 @@
+print(prop.test (2, 12))
+# kaheteistkümnest õpilasest on kaks poisid. Mida sellest järeldada võin? 
+# Vastus -  kui neid mõnel teisel kursusel 12 õpilase hulgas on 1-5, siis pole põhjust 
+# mingit eripära otsida
